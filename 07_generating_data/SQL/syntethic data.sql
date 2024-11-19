28c3cce519434e3c344a77b0bfd03c580d95b1df
Erik Unevik <erikunevik@gmail.com>
2024-11-19 15:40:25 +0100

    Härkommer 07

---
+++ 07_generating_data/SQL/syntethic data.sql
@@ -0,0 +1,37 @@
+
+-- Får 100 ID
+SELECT UNNEST (generate_series(1, 100)) as id; -- id namnger columnen bara
+
+SELECT RANDOM();
+
+-- Randomiserar till 6 men sen + 1 så upptill 7 pga 1an, AS dice upp till 100, floor avrundar
+
+SELECT FLOOR(RANDOM()*6+1) AS dice FROM generate_series(100);
+
+
+SELECT
+	UNNEST (generate_series(100)) AS id,
+	'student_' || id AS student,
+	ROUND(RANDOM()*100) AS score;
+
+SELECT
+	id,
+	'student_' || id AS student,
+	 score,
+	 CASE
+		 WHEN score < 50 THEN 'F'
+		 WHEN score < 75 THEN 'G'
+		 ELSE 'VG'
+	 END AS grade
+FROM
+	(
+	SELECT
+		UNNEST (generate_series(100)) AS id,
+		ROUND(RANDOM()*100) AS score
+	     );
+	    
+	    
+
+
+
+
