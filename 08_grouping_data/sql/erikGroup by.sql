98bfdcbe21717837e6e6e1e6cf8ab2929b19cd93
Erik Unevik <erikunevik@gmail.com>
2024-11-25 11:51:51 +0100

    Ändringar25112024föreläsning

--- 08_grouping_data/sql/erikGroup by.sql
+++ 08_grouping_data/sql/erikGroup by.sql
@@ -19,14 +19,12 @@ LIMIT 10;
 
 SELECT
 	food,
-	SUM(number_searches) AS total_searches
+	SUM(number_searches)
 FROM
 	main.cleaned_food
 GROUP BY
-	food
-ORDER BY
-	total_searches DESC
-LIMIT 10;
+	food;
+
 
 --- Fugnear inte heller
 
@@ -100,6 +98,63 @@ HAVING
     SUM(number_searchers) > 300000
 ORDER BY 
     SUM(number_searchers) DESC;
+
+
+   SELECT --3
+    food,
+    SUM(number_searchers) AS total_searchers
+FROM -- 1
+    main.cleaned_food
+GROUP BY  --2
+    food
+ORDER BY -- 4
+    total_searchers DESC
+LIMIT 10;
+
+
+DESCRIBE main.cleaned_food;
+
+
+--- 
+
+
+
+   
+SELECT 
+    year,
+    SUM(number_searchers) AS total_searchers
+FROM 
+    main.cleaned_food
+GROUP BY
+    year
+HAVING 
+    total_searchers > 300000
+ORDER BY 
+    total_searchers DESC;
+   
+SELECT
+	year,
+	food SUM(number_searches)
+FROM
+	main.cleaned_food
+GROUP BY
+	food,
+	year
+ORDER BY
+	total_searches DESC;
+
+SELECT
+    year,
+    food,
+    SUM(number_searchers) AS total_searches
+FROM
+    main.cleaned_food
+GROUP BY
+    food,
+    year
+ORDER BY
+    total_searches DESC;
+
 
 
 
